654bd744073f16817bc77b693a3131fc838cc04b
Mario Garcia <marioagarcia@knights.ucf.edu>
2022-10-25 21:56:55 -0500

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,14 +10,36 @@ public class PlayerScript : MonoBehaviour
     public float speed;
 
     public Text score;
+    public Text life;
 
     private int scoreValue = 0;
+    private int liveValue = 3;
+    private bool facingRight = true;
+    private bool isOnGround;
+
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
+    public AudioClip victoryMusic;
+    public AudioClip defeatMusic;
+    public AudioClip backgroundMusic;
+    public AudioSource musicSource;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        anim = GetComponent<Animator>();
+        
+        musicSource.clip = backgroundMusic;
+        musicSource.Play();
+        musicSource.loop = true;
+        SetCountText();
+        winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,6 +48,57 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        if(facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
+        if(isOnGround == true)
+        {
+            anim.SetInteger("State", 0);
+        }
+        else if(isOnGround == false)
+        {
+            anim.SetInteger("State", 2);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    void SetCountText()
+    {
+        score.text = "Score: " + scoreValue.ToString();
+        life.text = "Lives: " + liveValue.ToString();
+
+
+        if(scoreValue == 8)
+        {
+           winTextObject.SetActive(true);
+           musicSource.loop = false;
+           musicSource.clip = victoryMusic;
+           musicSource.Play();
+           musicSource.Stop();
+        }
+        else if(liveValue == 0)
+        {
+            loseTextObject.SetActive(true);
+            musicSource.loop = false;
+            musicSource.clip = defeatMusic;
+            musicSource.Play();
+            Destroy(gameObject, 5);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,15 +106,29 @@ public class PlayerScript : MonoBehaviour
        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
-        }
 
+            SetCountText();
+
+            if(scoreValue == 4)
+            {
+                transform.position = new Vector3(61.6f, 0.0f, 0.0f);
+                liveValue = 3;
+                SetCountText();
+            }
+        }
+        else if (collision.collider.tag == "Enemy")
+        {
+            liveValue -= 1;
+            Destroy(collision.collider.gameObject);
+
+            SetCountText();
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
             if (Input.GetKey(KeyCode.W))
             {
